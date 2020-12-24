4e62d5a4a871fa9ea14bcfdabd6783dcb9948cf4
Sam Seng <sseng1@bloomberg.net>
2020-12-24 11:54:25 -0500

    Play back audio recording

--- audio.py
+++ audio.py
@@ -12,10 +12,7 @@ RECORD_SECONDS = 0.1
 def callback(in_data, frame_count, time_info, flag, audio_callback):
     # len 2048 each element is an int from [0, 255]
     try:
-        as_arr = []
-        for byte in in_data:
-            as_arr.append(int(byte))
-        audio_callback.send(as_arr)
+        audio_callback.send(in_data)
     except:
         pass
     return in_data, pyaudio.paContinue
@@ -33,21 +30,33 @@ def record(audio_child_conn):
     print("Start recording", flush=True)
     stream.start_stream()
 
+    play_stream = p.open(format=FORMAT,
+        channels=CHANNELS,
+        rate=RATE,
+        output=True,
+        frames_per_buffer=CHUNK)
+
     while stream.is_active():
-        has_message = audio_child_conn.poll(0.1)
+        has_message = audio_child_conn.poll(0.01)
         if has_message:
             msg = audio_child_conn.recv()
-            print("Received! {0}".format(msg))
+            # print("Received! {0}".format(msg))
             if msg and msg["key"] == "exit":
-                #stream.stop_stream()
+                # If we don't close this before stopping the stream the process will hang
+                print("Closing stream", flush=True)
+                audio_child_conn.close()
+                stream.stop_stream()
                 break
+            if msg and msg["key"] == "play_audio":
+                play_stream.write(msg["data"])
 
     print("Done recording", flush=True)
 
-    #stream.close()
-    #p.terminate()
+    stream.close()
+    play_stream.close()
+    p.terminate()
 
     audio_child_conn.close()
 
-    print("Closed!")
+    print("Closed audio process", flush=True)
 
--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import numpy as np
 
 from multiprocessing import Process, Pipe
 
-
 import audio
 import sqlite_db
 
@@ -10,29 +9,26 @@ import sqlite_db
 # Master transfers
 # Each GPU holds state
 
-
-GLOBALS = {
-    "nodes": {},
-    "exit": False
-}
-def audio_callback(in_data):
+def audio_callback(in_data, nodes, weights):
     # len 2048 each element is an int from [0, 255]
-    global GLOBALS
     for i in range(len(in_data)):
-        GLOBALS["nodes"][i]["value"] = in_data[i]
-    tick()
+        nodes[i]["value"] = in_data[i]
+    tick(nodes, weights)
 
-def tick():
-    global GLOBALS
-    for node_id, node in GLOBALS["nodes"].items():
+s_tick = 0
+def tick(nodes, weights):
+    global s_tick
+    # print("Start tick {0}".format(s_tick), flush=True)
+    for node_id, node in nodes.items():
         if len(node["input_connections"]) == 0:
             continue
-        node_weights = GLOBALS["weights"][node_id]
+        node_weights = weights[node_id]
         input_array = np.zeros_like(node_weights)
-        populate_input_array_for_node(input_array, GLOBALS["nodes"], node)
-        new_node_val = np.dot(input_array, node_weights)
-        # Average this out - memory since the new value only slighly effects the current value
-        node["value"] = new_node_val
+        populate_input_array_for_node(input_array, nodes, node)
+        new_node_val = np.dot(input_array, node_weights) / len(node["input_connections"])
+        node["value"] = node["value"] * 0.9 + new_node_val * 0.1
+    # print("End tick {0}".format(s_tick), flush=True)
+    s_tick += 1
 
 
 def populate_input_array_for_node(input_array, all_nodes, node):
@@ -55,6 +51,15 @@ def get_nodes():
         "weights": weights
     }
 
+def get_audio_output(nodes):
+    output_nodes_keys = nodes.keys()
+    output_values = np.zeros((2048))
+    max_node_key = max(output_nodes_keys)
+    for i in range(2048):
+        node_id = max_node_key - 2048 + i
+        output_values[i] = round(nodes[node_id]["value"])
+    return output_values
+
 def print_node_values(nodes):
     print_ar = np.zeros((len(nodes)))
     idx = 0
@@ -75,31 +80,61 @@ def print_node_values(nodes):
 def audio_record_on_different_process(audio_child_conn):
     audio.record(audio_child_conn)
 
-def main():
-    global GLOBALS
+def data_manager_main(data_manager_child_conn):
     node_data = get_nodes()
-    GLOBALS["nodes"] = node_data["nodes"]
-    GLOBALS["weights"] = node_data["weights"]
+    nodes = node_data["nodes"]
+    weights = node_data["weights"]
 
     audio_parent_conn, audio_child_conn = Pipe()
     audio_process = Process(target=audio_record_on_different_process, args=(audio_child_conn,))
     audio_process.start()
 
     while True:
-        has_audio_data = audio_parent_conn.poll(0.1)
+        has_audio_data = audio_parent_conn.poll(0.01)
         if has_audio_data:
             audio_data = audio_parent_conn.recv()
-            print("Got audio data {0}".format(len(audio_data)))
-            audio_callback(audio_data)
-            break
+            print("Got audio_parent_conn: {0}".format(len(audio_data)))
+            audio_callback(audio_data, nodes, weights)
+            audio_output = get_audio_output(nodes)
+            # FIXME
+            audio_output = audio_data
+            audio_parent_conn.send({
+                "key": "play_audio",
+                "data": bytes(audio_output)
+            })
+
+        has_parent_data = data_manager_child_conn.poll(0.01)
+        if has_parent_data:
+            parent_data = data_manager_child_conn.recv()
+            # print("Got data_manager_child_conn data: {0}".format(parent_data))
+            if parent_data and parent_data["key"] == "exit":
+                break
 
     audio_parent_conn.send({
         "key": "exit"
     })
-    print_node_values(GLOBALS["nodes"])
+    print_node_values(nodes)
+    audio_process.join()
+    print("Closed data_manager_main", flush=True)
+
+def main():
+    # Command processor process
+
+    # Data manager process
+    data_manager_parent_conn, data_manager_child_conn = Pipe()
+    data_manager = Process(target=data_manager_main, args=(data_manager_child_conn,))
+    data_manager.start()
+
+    command = None
+    while command is not "e":
+        command = input("Enter command: ")
+
+    data_manager_parent_conn.send({
+        "key": "exit"
+    })
 
     print("Waiting to join processes")
-    audio_process.join()
+    data_manager.join()
     print("Joined processes")
 
 
